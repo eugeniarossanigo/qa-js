af76f632797365f1b996e5ef001ec617b837aad6
pikymga <eugeniarossanigo@gmail.com>
2021-03-29 17:01:28 -0300

    Add exercise 4

---
+++ exercise-4/index.js
@@ -0,0 +1,43 @@
+// If Else
+
+/** Exercise 4) a
+Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(),
+si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje
+“Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5” **/
+
+var num1 = Math.random();
+if (num1 >= 0.5) {
+    alert(num1 + " = Greater than 0,5");
+} else {
+    alert(num1 + " = Lower than 0,5");
+};
+
+
+/** Exercise 4) b
+Crear una variable “Age” que contenga un número entero entre 0 y 100
+y muestre los siguientes mensajes de alerta:
+
+- "Bebe" si la edad es menor a 2 años
+- "Nino" si la edad es entre 2 y 12 años
+- "Adolecente" si la edad es entre 13 y 19 años
+- "Joven" si la edad está entre 20 y 30 años
+- "Adulto" entre 31 y 60 años
+- "Adulto mayor" entre 61 y 75 años
+- "Anciano" si es mayor a 75 años **/
+
+var age = parseInt(Math.random()*100);
+if (age < 2) {
+    alert(age + " = Bebé");
+} else if (age >= 2 && age <= 12) {
+    alert(age + " = Niñx");
+} else if (age >= 13 && age <= 19) {
+    alert(age + " = Adolescente");
+} else if (age >= 20 && age <= 30) {
+    alert(age + " = Joven");
+} else if (age >= 31 && age <= 60) {
+    alert(age + " = Adultx");
+} else if (age >= 61 && age < 75) {
+    alert(age + " = Adultx mayor");
+} else {
+    alert(age + " = Ancianx");
+};
