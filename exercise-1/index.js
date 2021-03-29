fc05b8dbdd1581d17761290d757682523acee3e2
pikymga <eugeniarossanigo@gmail.com>
2021-03-29 16:59:51 -0300

    Add exercise 1

---
+++ exercise-1/index.js
@@ -0,0 +1,28 @@
+// Variables y Operadores
+
+/** Exercise 1) a
+Crear dos variables numéricas y utilizar el operador suma para
+guardar el valor de la suma de ambos números en una 3er variable. **/
+var num1 = 12;
+var num2 = 18;
+var totalSum = num1 + num2;
+console.log("The sum is:", totalSum);
+
+
+/** Exercise 1) b
+Crear dos variables de tipo String y concatenarlas
+guardando el resultado en una 3er variable. **/
+var firstName = "Eugenia";
+var lastName = "Rossanigo";
+var fullName = firstName +" "+ lastName;
+console.log("The concatenaded string is:", fullName);
+
+
+/** Exercise 1) c
+Crear dos variables de tipo String y sumar el largo de cada variable
+(cantidad de letras del string) guardando el resultado
+la suma una 3er variable (utilizar length). **/
+var str1 = "quality";
+var str2 = "assurance";
+var totalStrs = str1.length + str2.length;
+console.log("The lenght of the total string is:", totalStrs);
